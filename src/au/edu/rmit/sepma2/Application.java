609e2ef545f2960f7795eaa41d935a4c5b4d2a04
Tyson Horsewell <51278791+s3799530@users.noreply.github.com>
2020-10-07 14:34:06 +0800

    Ticketing
    Added:
    * Ticket class
    * Staff Admin
    * Show a ticket
    * Add a ticket
    * test data (user bob & password pwd)
    Updated:
    * String input regEx Validation
    * Integer Input now uses ArrayList for validation (easier to add tickets in Staff Admin)

--- src/au/edu/rmit/sepma2/Application.java
+++ src/au/edu/rmit/sepma2/Application.java
@@ -1,18 +1,22 @@
 package au.edu.rmit.sepma2;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Objects;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Main application class for Software Engineering Project Management Study Period 3
  * 2020, Group 12 Assignment 2
  * 
- * @author Rob Beardow      s3641721@student.rmit.edu.au
- * @author Tyson Horsewell  s3799530@student.rmit.edu.au
- * @author Jordon Edmondson s3779499@student.rmit.edu.au
+ * @author Rob Beardow ------ s3641721@student.rmit.edu.au
+ * @author Tyson Horsewell -- s3799530@student.rmit.edu.au
+ * @author Jordon Edmondson - s3779499@student.rmit.edu.au
  *
  */
 public class Application
@@ -21,6 +25,7 @@ public class Application
    private static final Scanner scanner = new Scanner(System.in);
 
    private final Set<User> users = new HashSet<>();
+   private List<Ticket> tickets = new ArrayList<>();
    private User currentUser = null;
 
    /**
@@ -28,10 +33,8 @@ public class Application
     */
    public static void main(final String[] args)
    {
-
-      final Application2 app = new Application();
+      final Application app = new Application();
       app.handleMainMenu();
-
    }
 
    protected Application()
@@ -39,10 +42,17 @@ public class Application
       initDefaultUserDatabase();
    }
 
+   /**
+    * Just shows the first menu level where someone can login, create account and
+    * rest their password
+    */
    private void handleMainMenu()
    {
+      //System.out.println(Role.values().length);
+      //System.out.println(Role.values()[0]);
+
       int selection;
-      int[] allowedMenuItems = { 1, 2, 3, 4 };
+      List<Integer> allowedMenuItems = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
       do
       {
          System.out.println("-------------------------------");
@@ -62,10 +72,10 @@ public class Application
                currentUser = null;
                break;
             case 2:
-               System.out.println("You selected create account");
+               createUser();
                break;
             case 3:
-               System.out.println("You selected reset password");
+               resetPassword();
                break;
             case 4:
                System.out.println("Exiting...");
@@ -74,6 +84,9 @@ public class Application
       } while (selection != 4);
    }
 
+   /**
+    * the login form
+    */
    protected void userLogin()
    {
       System.out.println("-------------------------------");
@@ -81,15 +94,17 @@ public class Application
       System.out.println("--------- User Login ----------");
       System.out.println("-------------------------------");
 
-      // ask for ID
       String id = getStringInput("ID: ");
+      // get the user that has that ID
       User user = findUserByID(id);
 
       if (id.equalsIgnoreCase(Objects.isNull(user) ? "" : user.getUsername()))
       {
+         // if the username matches get the password
          if (user.getPassword().equals(getStringInput("Password: ")))
          {
             currentUser = user;
+            staffloginArea();
          }
          else
          {
@@ -103,9 +118,142 @@ public class Application
       }
    }
 
+   /**
+    * Show the staff login area which is a form that includes their tickets and
+    * ability to add a new one
+    */
+   protected void staffloginArea()
+   {
+      int selection, i = 0, j = 3;
+      List<Integer> allowedMenuItems = new ArrayList<>(Arrays.asList(1, 2));
+      List<Ticket> myTickets = new ArrayList<>();
+
+      // get tickets
+      // add numbers to allowed menu items
+
+      do
+      {
+         System.out.println("-------------------------------");
+         System.out.println("----- IT Ticketing System -----");
+         System.out.println("-- Your Tickets :" + currentUser.getFirstName());
+         System.out.println("-------------------------------");
+         System.out.println("-- 1. Create a New Ticket");
+         System.out.println("-- 2. Logout");
+         System.out.println("-------------------------------");
+         if (!tickets.isEmpty())
+         {
+            i = 0;
+            for (Ticket ticket : tickets)
+            {
+               if (ticket.username.equalsIgnoreCase(currentUser.getUsername()))
+               {
+                  myTickets.add(ticket);
+                  System.out.println((i+j) + ". " + ticket.getSubmissionDate() + " - " +
+                                     ticket.getDescription());
+                  allowedMenuItems.add(j + i);
+                  i++;
+               }
+            }
+            System.out.println("-------------------------------");
+         }
+         selection = getIntInput("Your Selection: ", allowedMenuItems);
+         System.out.println();
+         switch (selection)
+         {
+            case 1:
+               createTicket();
+               break;
+            case 2:
+               // logout
+               break;
+            default:
+               showTicket(myTickets.get(selection - j));
+         }
+      } while (selection != 2);
+
+   }
 
    /**
-    *  Form to create a user
+    * Form to create a ticket
+    */
+   protected void createTicket()
+   {
+      System.out.println("-------------------------------");
+      System.out.println("----- IT Ticketing System -----");
+      System.out.println("---------- New Ticket ---------");
+      System.out.println("-------------------------------");
+
+      String fName = getStringInput("First Name: ");
+      String lName = getStringInput("Last Name: ");
+
+      // make sure the user name is unique
+      String id;
+      boolean exists;
+      User user;
+      do
+      {
+         id = getStringInput("ID: ");
+         user = findUserByID(id);
+         exists = id
+                  .equalsIgnoreCase(Objects.isNull(user) ? "" : user.getUsername());
+         if (!exists)
+         {
+            System.out.println("Error: That username does not exist.");
+         }
+      } while (!exists);
+
+      String contactNum =
+               getStringInput("Contact Number (02777 4444): ", "^\\+?[0-9 ]{8,14}$");
+
+      String submissionDate = getStringInput("Submission date (YYYY/MM/DD): ",
+                                             "^[0-9]{4}/[0-9]{2}/[0-9]{2}$");
+
+      String description = getStringInput("Description: ");
+
+      String severity =
+               getStringInput("Severity (LOW/MED/HIGH): ", "^(LOW|MED|HIGH)$");
+
+      Ticket t = new Ticket(fName, lName, id, contactNum, submissionDate,
+                            description, severity);
+      t.setIsOpen(true);
+      if (tickets.add(t))
+      {
+         System.out.println("New Open Ticket has been added to the system.");
+         System.out.println();
+      }
+   }
+
+   /**
+    * show an individual ticket after created to see progress
+    */
+   protected void showTicket(Ticket t)
+   {
+      List<Integer> allowedMenuItems = new ArrayList<>(Arrays.asList(1));
+      int selection;
+      System.out.println("-------------------------------");
+      System.out.println("----- IT Ticketing System -----");
+      System.out.println("----------- Ticket ------------");
+      System.out.println("-------------------------------");
+
+      System.out.println(t.getFirstName() + " " + t.getLastName() + " (" +
+                         t.getUserName() + ")");
+      System.out.println("Phone: " + t.getContactNumber());
+      System.out.println("Ticket Submitted: " + t.getSubmissionDate());
+      System.out.println("Ticket Description: \n" + t.getDescription());
+      System.out.println();
+      System.out.println("Severity" + t.getSeverity());
+
+      System.out.println("-------------------------------");
+      do
+      {
+         selection =
+                  getIntInput("Type \"1\" to return to admin: ", allowedMenuItems);
+         System.out.println();
+      } while (selection != 1);
+   }
+
+   /**
+    * Form to create a user
     */
    protected void createUser()
    {
@@ -116,7 +264,7 @@ public class Application
 
       String fName = getStringInput("First Name: ");
       String lName = getStringInput("Last Name: ");
-      
+
       // make sure the user name is unique
       String id;
       boolean exists;
@@ -136,7 +284,7 @@ public class Application
       String p = getStringInput("Password: ");
       Role r = getRoleInput("Role (1 = Staff, 2 = Tech level 1, 3 = Tech level 2): ");
 
-      if ( users.add(new User(id, p, fName, lName, r)) )
+      if (users.add(new User(id, p, fName, lName, r)))
       {
          System.out.println("New User has been added to the system.");
          System.out.println();
@@ -145,7 +293,7 @@ public class Application
    }
 
    /**
-    *  Form to reset the password
+    * Form to reset the password
     */
    protected void resetPassword()
    {
@@ -170,7 +318,14 @@ public class Application
                   .println("Error: There is no user by that username in the system.");
       }
    }
-   
+
+   /**
+    * Get a string input from the command-line
+    * 
+    * @param label
+    *           the description of what needs to be input
+    * @return a String of what was input
+    */
    protected String getStringInput(String label)
    {
       String s;
@@ -183,10 +338,48 @@ public class Application
       return s;
    }
 
+   /**
+    * Get a string input from the command-line
+    * 
+    * @param label
+    *           the description of what needs to be input regEx a regular expression
+    *           for a match validation
+    * @return a String of what was input
+    */
+   protected String getStringInput(String label, String regEx)
+   {
+      String s;
+
+      Pattern pattern = Pattern.compile(regEx, Pattern.CASE_INSENSITIVE);
+      Matcher matcher;
+      boolean isMatch;
+
+      do
+      {
+         s = getStringInput(label);
+         matcher = pattern.matcher(s);
+         isMatch = matcher.find();
+         if (!isMatch)
+         {
+            System.out.println("Error: The text is not in the correct format.");
+         }
+      } while (!isMatch);
+      return s;
+   }
+
+   /**
+    * Set the role
+    * 
+    * @param label
+    *           the description of what needs to be input
+    * @return the role that is selected
+    * 
+    * @see au.edu.rmit.sepma2.Role
+    */
    protected Role getRoleInput(String label)
    {
       int s;
-      int[] allowedValues = { 1, 2, 3 };
+      List<Integer> allowedValues = new ArrayList<>(Arrays.asList(1, 2, 3));
       Role r = null;
       do
       {
@@ -207,12 +400,21 @@ public class Application
       return r;
    }
 
-   protected int getIntInput(String label, int[] allowedIntegers)
+   /**
+    * gets an integer input from the user
+    * 
+    * @param label
+    *           the description of what needs to be input
+    * @param allowedIntegers
+    *           this is an array of integers that is allowed to be validated against
+    * @return
+    */
+   protected int getIntInput(String label, List<Integer> allowedIntegers)
    {
       String s;
       int val;
 
-      if (allowedIntegers.length == 0)
+      if (allowedIntegers.isEmpty())
       {
          s = getStringInput(label);
          val = Integer.parseInt(s);
@@ -236,13 +438,32 @@ public class Application
             {
                System.out
                         .println("Error: That is an invalid selection.\nPlease enter: " +
-                                 Arrays.toString(allowedIntegers));
+                                 allowedIntegers.toString());
             }
          } while (!inArray);
       }
       return val;
    }
 
+   /**
+    * gets an integer input from the user (we don't care about which)
+    * 
+    * @param label
+    *           the description of what needs to be input
+    * @return an int
+    */
+   protected int getIntInput(String label)
+   {
+      return getIntInput(label);
+   }
+
+   /**
+    * find the user in the users collection
+    * 
+    * @param id
+    *           is the username of the user
+    * @return the user object
+    */
    protected User findUserByID(String id)
    {
       for (User user : users)
@@ -267,6 +488,22 @@ public class Application
                          Role.TECHNICIAN_LEVEL2));
       users.add(new User("zmalik", "password", "Zayne", "Malik",
                          Role.TECHNICIAN_LEVEL2));
+
+      users.add(new User("bob", "pwd", "Bobby", "Bob-Bob",
+                         Role.STAFF));
+
+      tickets.add(new Ticket("Bobby", "Bob-Bob", "bob", "0496323145", "2020/7/7",
+                             "This is just some test data 0.", "LOW"));
+      tickets.add(new Ticket("Bobby", "Bob-Bob", "bob", "0496323145", "2020/10/7",
+                             "This is just some test data 1.", "LOW"));
+      tickets.add(new Ticket("Bobby", "Bob-Bob", "bob", "0496323145", "2020/10/4",
+                             "This is just some test data 2.", "MED"));
+      tickets.add(new Ticket("Bobby", "Bob-Bob", "bob", "0496323145", "2020/10/5",
+                             "This is just some test data 3.", "MED"));
+      tickets.add(new Ticket("Bobby", "Bob-Bob", "bob", "0496323145", "2020/10/6",
+                             "This is just some test data 4.", "HIGH"));
+      tickets.add(new Ticket("Bobby", "Bob-Bob", "bob", "0496323145", "2020/10/8",
+                             "This is just some test data 5.", "HIGH"));
    }
 
    private enum Role
@@ -274,10 +511,13 @@ public class Application
       STAFF, TECHNICIAN_LEVEL1, TECHNICIAN_LEVEL2
    }
 
+   /**
+    * The User class
+    */
    protected static class User
    {
 
-      private String username;
+      private final String username; // this will not change
       private String password;
       private String firstName;
       private String lastName;
@@ -298,16 +538,16 @@ public class Application
          this.role = role;
       }
 
+      public void setPassword(String password)
+      {
+         this.password = password;
+      }
+
       public String getUsername()
       {
          return username;
       }
 
-      public void setPassword(String password)
-      {
-         this.password = password;
-      }
-      
       public String getPassword()
       {
          return password;
@@ -330,4 +570,78 @@ public class Application
 
    }
 
+   protected static class Ticket
+   {
+
+      private final String firstName;
+      private final String lastName;
+      private final String username; // username
+      private final String contactNumber;
+      private final String submissionDate;
+      private final String description;
+      private boolean isOpen; // status
+      private String severity;
+
+      // Fill in the ticket
+      public Ticket(String firstName, String lastName, String id,
+                    String contactNumber, String submissionDate, String description,
+                    String severity)
+      {
+         super();
+         this.firstName = firstName;
+         this.lastName = lastName;
+         this.username = id; // username
+         this.contactNumber = contactNumber;
+         this.submissionDate = submissionDate;
+         this.description = description;
+         this.severity = severity;
+         this.isOpen = false;
+      }
+
+      public boolean isOpen()
+      {
+         return isOpen;
+      }
+
+      public void setIsOpen(boolean isOpen)
+      {
+         this.isOpen = isOpen;
+      }
+
+      public String getSeverity()
+      {
+         return severity;
+      }
+
+      public String getFirstName()
+      {
+         return firstName;
+      }
+
+      public String getLastName()
+      {
+         return lastName;
+      }
+
+      public String getUserName()
+      {
+         return username;
+      }
+
+      public String getContactNumber()
+      {
+         return contactNumber;
+      }
+
+      public String getSubmissionDate()
+      {
+         return submissionDate;
+      }
+
+      public String getDescription()
+      {
+         return description;
+      }
+
+   }
 }
